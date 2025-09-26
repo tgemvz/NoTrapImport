6ee7f92c5937492fc2fb83227ec54a7d9f10a212
TGemVZ <140729804+tgemvz@users.noreply.github.com>
2025-09-26 11:26:03 +0200

    added AspireApp AI Wrapper

---
+++ AspireApp/AspireAppAIWrapper/src/AspireAppAIWrapper.cs
@@ -0,0 +1,9 @@
+namespace AspireApp_AIWrapper;
+    public class AspireAppAIWrapper
+    {
+        public Task<string> GetProductDtoFromUrl(string url)
+        {
+            // implement "DataExtraction" Logic here
+        }
+       
+    }
---
+++ AspireApp/AspireAppAIWrapper/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AspireAppAIWrapper")]
+[assembly: AssemblyDescription("A class library for interacting with the Aspire application.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Your Company Name")]
+[assembly: AssemblyProduct("AspireAppAIWrapper Product Name")]
+[assembly: AssemblyCopyright("Copyright © Your Company Name")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("your-guid-here")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
