50887c12655253c6998daf72475ee2b5c76bcc0b
jwitvz <joel.wittwer@vzch.com>
2025-09-27 19:10:04 +0200

    remove unused ragservice

--- AspireApp/AspireApp.RagService/Models/DocumentResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AspireApp.RagService.Models
-{
-    public class DocumentResult
-    { 
-        public string Id { get; set; }
-        public string Text { get; set; }
-        public float Score { get; internal set; }
-    }
-}
